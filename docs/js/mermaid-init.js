6bee822a1c8dbee8bea596c73363a833386c65cd
IgorStellet <igoralemida.gran@gmail.com>
2025-09-22 19:46:11 -0300

    tryng mermaid

--- docs/js/mermaid-init.js
+++ docs/js/mermaid-init.js
@@ -1,9 +1,4 @@
-/* docs/js/mermaid-init.js */
-window.addEventListener("DOMContentLoaded", () => {
-  if (window.mermaid) {
-    window.mermaid.initialize({
-      startOnLoad: true,      // render anything fenced as ```mermaid
-      theme: "default"        // or "neutral", "dark", etc.
-    });
-  }
-});
+window.mermaid = {
+  startOnLoad: true,
+  securityLevel: "strict",
+};
